e557f823ce344d2757a5dc8ff47dfc87dd14b00e
Kent <khmakishima@ucdavis.edu>
2019-05-04 02:43:25 +0800

    Updates

---
+++ precache-manifest.bf5148b4f32746c33a6958847ca8bb0a.js
@@ -0,0 +1,242 @@
+self.__precacheManifest = [
+  {
+    "revision": "17616c33894ef808a162ccb0573a9b40",
+    "url": "/armadahome/static/media/linkedin-icon.17616c33.svg"
+  },
+  {
+    "revision": "b4e33614fa60234ee74e",
+    "url": "/armadahome/static/css/main.0fdc5375.chunk.css"
+  },
+  {
+    "revision": "3e2efca7f26024990e83",
+    "url": "/armadahome/static/js/runtime~main.17395e7a.js"
+  },
+  {
+    "revision": "fecce994ef9f250f348818ca2f2b3d98",
+    "url": "/armadahome/static/media/nunitosans-regular-webfont.fecce994.eot"
+  },
+  {
+    "revision": "fa0c8182c1fcf384a9cc",
+    "url": "/armadahome/static/js/2.83296a2a.chunk.js"
+  },
+  {
+    "revision": "3f22926e45f99c49f7ad5146499242c7",
+    "url": "/armadahome/static/media/OG_logo.3f22926e.png"
+  },
+  {
+    "revision": "5a320f9fe9f18616b6ccfd1cc6657d4f",
+    "url": "/armadahome/static/media/triangle-9.5a320f9f.png"
+  },
+  {
+    "revision": "85cd132038ba58316406d25e78cc2cdf",
+    "url": "/armadahome/static/media/Armada_logo_horiz_white.85cd1320.png"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/armadahome/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/armadahome/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/armadahome/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/armadahome/static/media/slick.f97e3bbf.svg"
+  },
+  {
+    "revision": "f9d781d877cb6064641efa81c7d45f5f",
+    "url": "/armadahome/static/media/hashing-systems.f9d781d8.png"
+  },
+  {
+    "revision": "0da6ba96fada74c6acb30fe459e351b8",
+    "url": "/armadahome/static/media/hedera-hashgraph.0da6ba96.png"
+  },
+  {
+    "revision": "1da13c4f40147bbbc07a7092e3d32620",
+    "url": "/armadahome/static/media/mind-fund.1da13c4f.png"
+  },
+  {
+    "revision": "b784a69a285341600bf789558a9e7ced",
+    "url": "/armadahome/static/media/product-1.b784a69a.png"
+  },
+  {
+    "revision": "f8518f2d5514709cd1a82c0c01cbcbfc",
+    "url": "/armadahome/static/media/vector.f8518f2d.svg"
+  },
+  {
+    "revision": "b3440011fd940136eadc5b45209010d6",
+    "url": "/armadahome/static/media/header-pattern.b3440011.png"
+  },
+  {
+    "revision": "d0e48a3e6646b0f238366718ee83e5f5",
+    "url": "/armadahome/static/media/feature-image-1.d0e48a3e.svg"
+  },
+  {
+    "revision": "6b60486d374de12ab42dc41ef88d6211",
+    "url": "/armadahome/static/media/feature-image-2.6b60486d.svg"
+  },
+  {
+    "revision": "34e9171f6b102d0d3410aa5e3b4e29ab",
+    "url": "/armadahome/static/media/feature-image-3.34e9171f.svg"
+  },
+  {
+    "revision": "15c651ec362df1b379bd9826ed17f458",
+    "url": "/armadahome/static/media/triangle-11.15c651ec.png"
+  },
+  {
+    "revision": "6e62afd6aed27b7763993ca98bdf0ef4",
+    "url": "/armadahome/static/media/helix.6e62afd6.png"
+  },
+  {
+    "revision": "303d869e18e6dcde2385b23fa824255e",
+    "url": "/armadahome/static/media/yc-startup-school.303d869e.png"
+  },
+  {
+    "revision": "6c0241abfdac96c392e46514722570c8",
+    "url": "/armadahome/static/media/zs-blockchain.6c0241ab.png"
+  },
+  {
+    "revision": "d97a4203468b123c855c995d7a139b63",
+    "url": "/armadahome/static/media/product-2.d97a4203.png"
+  },
+  {
+    "revision": "4235d5d33256721ae3e51460915b7c62",
+    "url": "/armadahome/static/media/laptop-phone.4235d5d3.png"
+  },
+  {
+    "revision": "fe1edaadeb40db0b9176e31306d58f25",
+    "url": "/armadahome/static/media/footer-pattern.fe1edaad.png"
+  },
+  {
+    "revision": "e19688fa4f1d730c4878560e2558a3c8",
+    "url": "/armadahome/static/media/fb-icon.e19688fa.svg"
+  },
+  {
+    "revision": "6a18723d7438dd61aaace88ba79b5b00",
+    "url": "/armadahome/static/media/twitter-icon.6a18723d.svg"
+  },
+  {
+    "revision": "b4e33614fa60234ee74e",
+    "url": "/armadahome/static/js/main.0c2fc3d0.chunk.js"
+  },
+  {
+    "revision": "a9803b6d7affed2c66a90fa91dfe5b52",
+    "url": "/armadahome/static/media/nunitosans-black-webfont.a9803b6d.woff2"
+  },
+  {
+    "revision": "c5c65ea7356c12b4403443ad114950b4",
+    "url": "/armadahome/static/media/nunitosans-bold-webfont.c5c65ea7.woff2"
+  },
+  {
+    "revision": "9bc1d1f36e021a97b2129b7d9d9010e2",
+    "url": "/armadahome/static/media/nunitosans-extrabold-webfont.9bc1d1f3.woff2"
+  },
+  {
+    "revision": "d3aa9a1d8a86e83544b8ce75c399e8ed",
+    "url": "/armadahome/static/media/nunitosans-black-webfont.d3aa9a1d.eot"
+  },
+  {
+    "revision": "fa94421ec0786fbab048de0373003b1f",
+    "url": "/armadahome/static/media/nunitosans-extralight-webfont.fa94421e.woff2"
+  },
+  {
+    "revision": "50b6a4ee45c30cff3889f4f8aa6365e3",
+    "url": "/armadahome/static/media/nunitosans-bold-webfont.50b6a4ee.eot"
+  },
+  {
+    "revision": "0c7424feaa3d009cd9499f06e825ef2d",
+    "url": "/armadahome/static/media/nunitosans-light-webfont.0c7424fe.woff2"
+  },
+  {
+    "revision": "a3e098c0d6243ae390c72492f9872f14",
+    "url": "/armadahome/static/media/nunitosans-extrabold-webfont.a3e098c0.eot"
+  },
+  {
+    "revision": "efd839ec302ebeedb53bfe08dd61454c",
+    "url": "/armadahome/static/media/nunitosans-regular-webfont.efd839ec.woff2"
+  },
+  {
+    "revision": "fb4cbafd40aaec8b09df786b5bba6b7c",
+    "url": "/armadahome/static/media/nunitosans-semibold-webfont.fb4cbafd.woff2"
+  },
+  {
+    "revision": "5461847624baa2c30371faa0454749f4",
+    "url": "/armadahome/static/media/nunitosans-semibold-webfont.54618476.eot"
+  },
+  {
+    "revision": "0a5a148124f57666849b82b404a2c48b",
+    "url": "/armadahome/static/media/nunitosans-black-webfont.0a5a1481.woff"
+  },
+  {
+    "revision": "7bf82501477ab0d4bd119433f5af67ce",
+    "url": "/armadahome/static/media/nunitosans-extralight-webfont.7bf82501.eot"
+  },
+  {
+    "revision": "00e9f79b2cca1d06b93c4365f6aa7290",
+    "url": "/armadahome/static/media/nunitosans-light-webfont.00e9f79b.eot"
+  },
+  {
+    "revision": "91a3b7aa223e083c009398a72f9a51fe",
+    "url": "/armadahome/static/media/nunitosans-extrabold-webfont.91a3b7aa.woff"
+  },
+  {
+    "revision": "417a4640ea70c9a83c8ec4273a27026d",
+    "url": "/armadahome/static/media/nunitosans-extralight-webfont.417a4640.woff"
+  },
+  {
+    "revision": "3292bea5d61c40a7e2416bb6723ac86b",
+    "url": "/armadahome/static/media/nunitosans-bold-webfont.3292bea5.woff"
+  },
+  {
+    "revision": "d84e8f96a17ba9bf877e02fa072ba941",
+    "url": "/armadahome/static/media/nunitosans-light-webfont.d84e8f96.woff"
+  },
+  {
+    "revision": "3af8c1d07597ec2797fae1b9a6d80838",
+    "url": "/armadahome/static/media/nunitosans-regular-webfont.3af8c1d0.woff"
+  },
+  {
+    "revision": "5ec53d5613ec070b0d82b38509aed41e",
+    "url": "/armadahome/static/media/nunitosans-semibold-webfont.5ec53d56.woff"
+  },
+  {
+    "revision": "4f828ab31235e056254228417915a8c0",
+    "url": "/armadahome/static/media/nunitosans-bold-webfont.4f828ab3.ttf"
+  },
+  {
+    "revision": "96a3e72764bd79cc2f941f11587a5238",
+    "url": "/armadahome/static/media/nunitosans-black-webfont.96a3e727.ttf"
+  },
+  {
+    "revision": "3766912da5168b0d361b5a05a490004f",
+    "url": "/armadahome/static/media/nunitosans-extrabold-webfont.3766912d.ttf"
+  },
+  {
+    "revision": "028fd38a1c8ca5c92c979f2dfc7a172b",
+    "url": "/armadahome/static/media/nunitosans-light-webfont.028fd38a.ttf"
+  },
+  {
+    "revision": "65d50734b5334c70d0189010b59bef93",
+    "url": "/armadahome/static/media/nunitosans-extralight-webfont.65d50734.ttf"
+  },
+  {
+    "revision": "af0fb82e4a6b597fec9cc19ceda4ba10",
+    "url": "/armadahome/static/media/nunitosans-semibold-webfont.af0fb82e.ttf"
+  },
+  {
+    "revision": "b81fbe5b802ec79b035f34147798766c",
+    "url": "/armadahome/static/media/nunitosans-regular-webfont.b81fbe5b.ttf"
+  },
+  {
+    "revision": "fa0c8182c1fcf384a9cc",
+    "url": "/armadahome/static/css/2.d77d7c97.chunk.css"
+  },
+  {
+    "revision": "fac4fa3fd750f4ed7363de918d2a2653",
+    "url": "/armadahome/index.html"
+  }
+];
